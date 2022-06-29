2f54fc65457fcad94e26f73581481803349ca5a4
EduardXI <eduardo.salazar@agenciaservitec.com>
2022-06-28 19:13:20 -0500

    add img convenios

--- src/images/index.js
+++ src/images/index.js
@@ -1,2 +1,3 @@
 export { default as CobieneLogo } from "./others/cobiene-logo-large.webp";
 export { default as ApoyoEducativo } from "./others/apoyo-educativo.webp";
+export { default as Convenios } from "./others/convenios.webp";

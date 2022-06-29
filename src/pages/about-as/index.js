a59903a2c703e04b706b3ddf0584f163b3f1a800
EduardXI <eduardo.salazar@agenciaservitec.com>
2022-06-29 15:05:48 -0500

    add img

--- src/pages/about-as/index.js
+++ src/pages/about-as/index.js
@@ -1,11 +1,10 @@
 import React from "react";
 import { Barracks, Servitec } from "../../images";
 import { Button } from "../../components";
-import styled, { css } from "styled-components";
 
 export const AboutAs = () => {
   return (
-    <Container>
+    <cotainer>
       <div className="content-log">
         <img src={Barracks} alt="base" />
         <h2>NOSOTROS</h2>
@@ -13,38 +12,7 @@ export const AboutAs = () => {
       <Button width="100%">PRESENTACIÓN</Button>
       <Button width="100%">MISIÓN Y VISIÓN</Button>
       <Button width="100%">INFORMACION Y SOPORTE</Button>
-      <img className="img-servitec" src={Servitec} alt="servitec" />
-    </Container>
+      <img src={Servitec} alt="servitec" />
+    </cotainer>
   );
 };
-
-const Container = styled.div`
-  ${({ theme }) => css`
-    width: 100%;
-    height: auto;
-    .content-logo {
-      display: flex;
-      flex-direction: column;
-      align-items: center;
-      justify-content: center;
-      margin: 1rem auto;
-      img {
-        width: 8em;
-        height: auto;
-        object-fit: contain;
-        margin-bottom: 1rem;
-      }
-      h2 {
-        text-shadow: ${theme.text_shadow.shadow_green};
-      }
-      p {
-        color: ${theme.colors.white};
-        text-align: center;
-      }
-    }
-    img .img-servitec {
-      height: 10px;
-      width: 10px;
-    }
-  `}
-`;

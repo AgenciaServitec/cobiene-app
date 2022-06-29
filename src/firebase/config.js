7dfe5b4513a4b63648c63f23aa6e078c29f51e07
noelmoriano <noel.moriano@webuild.pe>
2022-06-29 15:04:34 -0500

    refactoring project

--- src/firebase/config.js
+++ src/firebase/config.js
@@ -1,7 +1,3 @@
-import firebase from "firebase/app";
-import "firebase/firestore";
-import "firebase/auth";
-import "firebase/storage";
 import configs from "./configs.json";
 import { includes } from "lodash";
 
@@ -18,46 +14,8 @@ const currentConfig = configs[currentEnvironment];
 const common = configs.common;
 const contactData = configs.common.contactData;
 
-const { firebaseApp, version, apiUrl, ipInfoApi } = currentConfig;
+const { version, apiUrl, ipInfoApi } = currentConfig;
 
 console.log(currentEnvironment, ":", version);
 
-firebase.initializeApp(firebaseApp);
-
-const auth = firebase.auth();
-const firestore = firebase.firestore();
-const storage = firebase.storage();
-
-firestore.enablePersistence().then(() => console.log("Persistence enabled"));
-
-const buckets = {
-  default: storage,
-};
-
-Object.keys(currentConfig.buckets).forEach((bucketKey) => {
-  buckets[bucketKey] = firebase.app().storage(currentConfig.buckets[bucketKey]);
-});
-
-let pageLoaded = false;
-
-firestore.doc("settings/default").onSnapshot(() => {
-  pageLoaded && document.location.reload(true);
-  pageLoaded = true;
-});
-
-const imageResizes = ["400x500", "1400x600"];
-
-export {
-  firebase,
-  firestore,
-  auth,
-  storage,
-  buckets,
-  version,
-  apiUrl,
-  ipInfoApi,
-  currentConfig,
-  imageResizes,
-  common,
-  contactData,
-};
+export { version, apiUrl, ipInfoApi, currentConfig, common, contactData };
---
+++ src/pages/conventions/universities/index.js
@@ -0,0 +1,32 @@
+import React from "react";
+import { Button } from "../../../components";
+import styled, { css } from "styled-components";
+
+export const Universities = () => {
+  return (
+    <Container>
+      <div className="content-logo">
+        <h2>Universidades</h2>
+      </div>
+      <Button width="100%">hi</Button>
+    </Container>
+  );
+};
+
+const Container = styled.div`
+  ${({ theme }) => css`
+    width: 100%;
+    height: auto;
+    .content-logo {
+      display: flex;
+      flex-direction: column;
+      align-items: center;
+      justify-content: center;
+      margin: 1rem auto;
+
+      h2 {
+        text-align: center;
+      }
+    }
+  `}
+`;

11024693848574b557d33a7486fb0ec6ec372279
angelgt <angel.gamonal@agenciaservitec.com>
2022-07-06 11:52:53 -0500

    add animation

--- src/components/layout/user/BaseLayout.js
+++ src/components/layout/user/BaseLayout.js
@@ -9,6 +9,7 @@ export const BaseLayout = ({ children }) => {
 
   return (
     <Container>
+      <div className="overlay"></div>
       <Drawer
         visibleDrawer={visibleDrawer}
         onSetVisibleDrawer={setVisibleDrawer}
@@ -27,6 +28,54 @@ const Container = styled.div`
   position: relative;
   background: radial-gradient(#11581e, #041607),
     url("https://media.giphy.com/media/oEI9uBYSzLpBK/giphy.gif");
+  z-index: 1;
+  .overlay {
+    pointer-events: none;
+    position: absolute;
+    width: 100%;
+    height: 100%;
+    background: repeating-linear-gradient(
+      180deg,
+      rgba(0, 0, 0, 0) 0,
+      rgba(0, 0, 0, 0.3) 50%,
+      rgba(0, 0, 0, 0) 100%
+    );
+    background-size: auto 4px;
+    z-index: -1;
+
+    &:before {
+      content: "";
+      pointer-events: none;
+      position: absolute;
+      display: block;
+      top: 0;
+      left: 0;
+      right: 0;
+      bottom: 0;
+      width: 100%;
+      height: 100%;
+      background-image: linear-gradient(
+        0deg,
+        transparent 0%,
+        rgba(32, 128, 32, 0.2) 2%,
+        rgba(32, 128, 32, 0.8) 3%,
+        rgba(32, 128, 32, 0.2) 3%,
+        transparent 100%
+      );
+      background-repeat: no-repeat;
+      animation: scan 7.5s linear 0s infinite;
+    }
+    @keyframes scan {
+      0% {
+        background-position: 0 -100vh;
+      }
+
+      35%,
+      100% {
+        background-position: 0 100vh;
+      }
+    }
+  }
   .body {
     width: 100%;
     height: auto;

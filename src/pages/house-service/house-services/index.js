f948ebc518eae82c0673cb5010c6371a29de576b
angelgt <angel.gamonal@agenciaservitec.com>
2022-08-28 16:06:11 -0500

    update page

--- src/pages/house-service/house-services/index.js
+++ src/pages/house-service/house-services/index.js
@@ -1,113 +1,78 @@
-import React, { useState } from "react";
-import styled, { css } from "styled-components";
+import React from "react";
+import styled from "styled-components";
 import {
   ImgLogoJace,
   ImgSliderJace,
   ImgSliderJace2,
   ImgSliderJace3,
 } from "../../../images";
-import { Button, NavigateGoToLink, Text } from "../../../components";
+import {
+  Button,
+  InitialContent,
+  NavigateGoToLink,
+  ShowAndHiddenButton,
+} from "../../../components";
 import Carousel from "antd/lib/carousel";
+import { faFileInvoice, faLightbulb } from "@fortawesome/free-solid-svg-icons";
+import { requerimentsJace } from "../../../data-list";
 
-export const HouseServices = () => {
-  const [buttonHidden, setButtonHidden] = useState(false);
+export const HouseServices = () => (
+  <Container>
+    <InitialContent
+      title="Jace"
+      img={ImgLogoJace}
+      description=" La JACE asesora al Comando de Bienestar del Ejército en los aspectos
+            inherentes de la administración de casas de servicios del Ejército.
+            Asimismo, planificara, ejecutara y supervisara los trabajos de
+            conservación, mejoramiento y rehabilitación de la infraestructura,
+            inmobiliaria de las casas de servicio de la guarnición de Lima, a fin
+            de contribuir a la consecución de los objetivos de Bienestar del
+            Ejército."
+    />
 
-  return (
-    <Container>
-      <div className="content-logo">
-        <img src={ImgLogoJace} alt="Casa de Servicio" />
-        <h2>Jace</h2>
-        <Text>
-          La JACE asesora al Comando de Bienestar del Ejército en los aspectos
-          inherentes de la administración de casas de servicios del Ejército.
-          Asimismo, planificara, ejecutara y supervisara los trabajos de
-          conservación, mejoramiento y rehabilitación de la infraestructura,
-          inmobiliaria de las casas de servicio de la guarnición de Lima, a fin
-          de contribuir a la consecución de los objetivos de Bienestar del
-          Ejército.
-        </Text>
-      </div>
+    <Carousel autoplay>
+      <img src={ImgSliderJace} alt="Img Jace" className="img-mapa" />
+      <img src={ImgSliderJace2} alt="Img Jace" className="img-mapa" />
+      <img src={ImgSliderJace3} alt="Img Jace" className="img-mapa" />
+    </Carousel>
 
-      <Carousel autoplay>
-        <img src={ImgSliderJace} alt="Img Jace" className="img-mapa" />
-        <img src={ImgSliderJace2} alt="Img Jace" className="img-mapa" />
-        <img src={ImgSliderJace3} alt="Img Jace" className="img-mapa" />
-      </Carousel>
+    <div className="section-requirements">
+      <h3>Requisitos para ser usuario:</h3>
 
-      <Button width="100%" onClick={() => setButtonHidden(!buttonHidden)}>
-        Requisitos para ser usuarios
-      </Button>
-      {buttonHidden && (
-        <div className="content">
-          <p>
-            - Deberá encontrar en situación COBIENE de actividad con no mas de
-            25 años de único como oficial de 28 años para el caso de los
-            teóricos.
-            <br />- Deberá ser casado (a) mantener una relación conyugal estable
-            y vivir con su cónyuge e hijos en la guarnición donde solicita
-            vivienda. - <br />
-            El titular, esposa y familiares directos deberán estar debidamente
-            inscritos en el registro de Familia del COBIENE.
-            <br />
-            - Deberá tener como mínimo cuatro (04) años de servicio cumplido
-            como oficial, en el grado mínimo de teniente y SO2 para el caso de
-            los SSOO.
-            <br /> - Deberá prestar servicio en la guarnición donde solicita
-            vivienda.
-            <br /> - No debe habar ocupado casa de servicio en los tres (3)
-            últimos años en la guarnición donde se solicita. <br /> - No haber
-            sido favorecido con un programa (vivienda/préstamo) del ORES-FOVINE.
-            <br /> -Acreditara haber devuelto la casa de servicio que haya
-            ocupado en la Guarnición de procedencia.
-          </p>
-        </div>
-      )}
+      {/* {requerimentsJace.map((requerimentJace, index) => (
+        <ShowAndHiddenButton
+          key={index}
+          title={requerimentJace.title}
+          icon={requerimentJace.icon}
+          description={requerimentJace.description}
+        />
+      ))}*/}
+      <ShowAndHiddenButton requerimentsJace={requerimentsJace} />
+    </div>
 
-      <Button
-        width="100%"
-        onClick={() =>
-          NavigateGoToLink(
-            "https://drive.google.com/file/d/1BMuifWm1NTDkhUtynifpoRzutQiQ35Uw/view?usp=sharing"
-          )
-        }
-      >
-        Descargar Solicitud
-      </Button>
-    </Container>
-  );
-};
+    <Button
+      type="secondary"
+      width="100%"
+      onClick={() =>
+        NavigateGoToLink(
+          "https://drive.google.com/file/d/1BMuifWm1NTDkhUtynifpoRzutQiQ35Uw/view?usp=sharing"
+        )
+      }
+    >
+      Descargar Solicitud
+    </Button>
+  </Container>
+);
 
 const Container = styled.div`
-  ${({ theme }) => css`
-    width: 100%;
-    height: auto;
+  width: 100%;
+  height: auto;
 
-    .content-logo {
-      display: flex;
-      flex-direction: column;
-      align-items: center;
-      justify-content: center;
-      margin: 1rem auto;
-
-      img {
-        width: 8em;
-        height: auto;
-        object-fit: contain;
-        margin-bottom: 1rem;
-      }
-
-      h2 {
-        text-shadow: ${theme.text_shadow.shadow_green};
-      }
-
-      p {
-        color: ${theme.colors.white};
-        text-align: justify;
-      }
-    }
-  `}
   .img-mapa {
     width: 100%;
     max-width: 24rem;
+  }
+  .section-requirements {
+    padding: 2.5em 0.5em 2em 0.5em;
   }
 `;

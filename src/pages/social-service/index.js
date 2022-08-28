f611c683e11838472bd6fa21487310a106d0cb98
angelgt <angel.gamonal@agenciaservitec.com>
2022-08-28 17:44:30 -0500

    refactor code

--- src/pages/social-service/index.js
+++ src/pages/social-service/index.js
@@ -1,20 +1,18 @@
 import React from "react";
 import { useNavigate } from "react-router";
 import { Service } from "../../images";
-import { Button } from "../../components";
-import styled, { css } from "styled-components";
+import { Button, InitialContent } from "../../components";
+import styled from "styled-components";
 
 export const ServiceSocial = () => {
   const navigate = useNavigate();
 
   return (
     <Container>
-      <div className="content-logo">
-        <img src={Service} alt="Servicio-social" />
-        <h2>APOYO SOCIAL</h2>
-        <p>
-          <br />
-          El departamento de apoyo social como parte del comando de bienestar
+      <InitialContent
+        img={Service}
+        title="Apoyo Social"
+        description="El departamento de apoyo social como parte del comando de bienestar
           del ejército, realiza la suscripción de convenios de educación
           superior, descuentos por convenios en pensiones de estudios
           superiores, otorgamiento de becas, ayudas económicas para casos
@@ -22,9 +20,8 @@ export const ServiceSocial = () => {
           de servicio religioso, asesoramiento de apoyo a la familia y apoyo al
           personal discapacitado, orientados a lograr beneficios para el
           personal militar, civil en actividad, retiro, cesantes y sus
-          familiares directos.
-        </p>
-      </div>
+          familiares directos."
+      />
       <Button
         width="100%"
         onClick={() => navigate("/social-service/economical-support")}
@@ -47,35 +44,7 @@ export const ServiceSocial = () => {
   );
 };
 
-const Container = styled.div`
-  ${({ theme }) => css`
-    width: 100%;
-    height: auto;
-    .content-logo {
-      display: flex;
-      flex-direction: column;
-      align-items: center;
-      justify-content: center;
-      margin: 1rem auto;
-
-      h2 {
-        text-align: center;
-        text-shadow: 0 0 1ex rgb(51 255 51), 0 0 2px rgb(255 255 255 / 80%);
-      }
-      img {
-        width: 8em;
-        height: auto;
-        object-fit: contain;
-        margin-bottom: 1rem;
-      }
-      p {
-        color: ${theme.colors.white};
-        text-align: justify;
-        font-size: 17px;
-      }
-    }
-    .link-list {
-      padding: 1rem;
-    }
-  `}
+const Container = styled.section`
+  width: 100%;
+  height: auto;
 `;

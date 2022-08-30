6e9d0499a2af9fef7d4145d79342da046f934713
angelgt <angel.gamonal@agenciaservitec.com>
2022-08-30 11:31:57 -0500

    add list string

--- src/data-list/requerimentsJace.js
+++ src/data-list/requerimentsJace.js
@@ -14,55 +14,61 @@ export const requerimentsJace = [
   {
     title: "Paso 1",
     icon: faFileInvoice,
-    description:
+    descriptions: [
       "Deberá encontrarse en situación militar de actividad con no más de 25 años para Oficiales y 28 años para Tcos y Sub Oficiales.",
+    ],
   },
   {
     title: "Paso 2",
     icon: faLightbulb,
-    description:
+    descriptions: [
       "Deberá ser casado y tener hijos con su conyugue, mantener una relación estable y vivir con su conyugue en la guarnición donde solicita la casa.",
+    ],
   },
   {
     title: "Paso 3",
     icon: faComments,
-    description:
+    descriptions: [
       "Deberá tener como mínimo 4 años de servicio cumplido como Oficial o Sub Oficial, con el grado mínimo de Teniente o Sub Oficial 2o.",
+    ],
   },
   {
     title: "Paso 4",
     icon: faCloud,
-    description:
+    descriptions: [
       "El personal militar que proviene de la reserva puede solicitar asignación de casa de servicio a partir de la fecha que pasen a la efectividad",
+    ],
   },
   {
     title: "Paso 5",
     icon: faPeopleGroup,
-    description:
+    descriptions: [
       "No deben haber sido favorecidos con un préstamo de Ores –Fovime.",
+    ],
   },
   {
     title: "Paso 6",
     icon: faGears,
-    description:
+    descriptions: [
       "No debe haber ocupado casa de servicio en los 3 últimos años en la guarnición donde solicita.",
+    ],
   },
   {
     title: "Paso 7",
-    icon: faFileSignature,
-    description:
-      "El personal militar que proviene de la reserva puede solicitar asignación de casa de servicio a partir de la fecha que pasen a la efectividad.",
+    icon: faFlag,
+    descriptions: [
+      "El personal militar candidato a usuario no debe haber sido obligado a desocupar la casa de servicio en cualquiera de las guarniciones de Ejercito donde existan casas de servicio.",
+    ],
   },
   {
     title: "Paso 8",
-    icon: faFlag,
-    description:
-      "El personal militar candidato a usuario no debe haber sido obligado a desocupar la casa de servicio en cualquiera de las guarniciones de Ejercito donde existan casas de servicio.",
-  },
-  {
-    title: "Paso 9",
     icon: faPuzzlePiece,
-    description:
-      "El personal militar candidato a “usuario” que solicite asignación especial o excepcional deberá contar con:Los documentos que acrediten la situación de especialidad o excepcionalidad.El dictamen legal favorable.La opinión del Crl Ing Jefe de la DACE.La aprobación del General de Brigada Comandante General del COBIENE, para la guarnición de Lima.",
+    descriptions: [
+      "El personal militar candidato a “usuario” que solicite asignación especial o excepcional deberá contar con:",
+      " - Los documentos que acrediten la situación de especialidad o excepcionalidad.",
+      " - El dictamen legal favorable.",
+      " - La opinión del Crl Ing Jefe de la DACE.",
+      " - La aprobación del General de Brigada Comandante General del COBIENE, para la guarnición de Lima.",
+    ],
   },
 ];

19c92e8e96015778fc2d3f96dbc2139c6e10a322
angelgt <angel.gamonal@agenciaservitec.com>
2022-08-30 11:29:47 -0500

    add list requeriment

--- src/components/home/ShowAndHiddenButton.js
+++ src/components/home/ShowAndHiddenButton.js
@@ -11,28 +11,35 @@ const { Text } = Typography;
 export const ShowAndHiddenButton = ({ requerimentsJace = [] }) => {
   return (
     <CollapseAnt accordion bordered={false} expandIcon={null}>
-      {requerimentsJace.map((requeriment, index) => (
-        <Panel
-          key={index}
-          header={
-            <Button width="100%" type="tertiary">
-              <div className="content-button">
-                <div className="item-img">
-                  <FontAwesomeIcon icon={requeriment.icon} />
+      {requerimentsJace.map((requeriment, index) => {
+        console.log("requeriment", requeriment);
+        return (
+          <Panel
+            key={index}
+            header={
+              <Button width="100%" type="tertiary">
+                <div className="content-button">
+                  <div className="item-img">
+                    <FontAwesomeIcon icon={requeriment.icon} />
+                  </div>
+                  <div className="item-text">
+                    <Text>
+                      <h3>{requeriment.title}</h3>
+                    </Text>
+                  </div>
                 </div>
-                <div className="item-text">
-                  <Text>
-                    <h3>{requeriment.title}</h3>
-                  </Text>
-                </div>
-              </div>
-            </Button>
-          }
-          className="site-collapse-custom-panel"
-        >
-          <p className="description">{requeriment.description}</p>
-        </Panel>
-      ))}
+              </Button>
+            }
+            className="site-collapse-custom-panel"
+          >
+            <ul className="description">
+              {requeriment.descriptions.map((description, index) => (
+                <li key={index}>{description}</li>
+              ))}
+            </ul>
+          </Panel>
+        );
+      })}
     </CollapseAnt>
   );
 };
@@ -59,9 +66,13 @@ const CollapseAnt = styled(Collapse)`
     }
   }
   .description {
-    font-size: 1.5em;
+    font-size: 1.3em;
     line-height: 1.3em;
     padding: 0 0.5em;
     text-align: justify;
+    list-style: none;
+    li {
+      margin-bottom: 0.5em;
+    }
   }
 `;

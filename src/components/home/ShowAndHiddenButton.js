09beb0f5ed09124a4e47f601b66e1f5e8c35eef7
angelgt <angel.gamonal@agenciaservitec.com>
2022-09-08 15:43:38 -0500

    refactor code

--- src/components/home/ShowAndHiddenButton.js
+++ src/components/home/ShowAndHiddenButton.js
@@ -1,30 +1,45 @@
 import React from "react";
-import styled from "styled-components";
+import styled, { css } from "styled-components";
 import Collapse from "antd/lib/collapse";
 import Typography from "antd/lib/typography";
-import { Button } from "../ui";
+import { Button, SocialLink } from "../ui";
 import { FontAwesomeIcon } from "@fortawesome/react-fontawesome";
+import Carousel from "antd/lib/carousel";
+import { ImgCarousel } from "./ImgCarousel";
 
 const { Panel } = Collapse;
 const { Text } = Typography;
 
-export const ShowAndHiddenButton = ({ requerimentsJace = [] }) => {
+export const ShowAndHiddenButton = ({
+  dataLists = [],
+  buttonType = "primary",
+  contentAlign = "start",
+  gripTemplateColumns = "1fr",
+}) => {
   return (
-    <CollapseAnt accordion bordered={false} expandIcon={null}>
-      {requerimentsJace.map((requeriment, index) => {
-        // console.log("requeriment", requeriment);
+    <CollapseAnt
+      accordion
+      bordered={false}
+      expandIcon={null}
+      contentAlign={contentAlign}
+      gripTemplateColumns={gripTemplateColumns}
+    >
+      {dataLists.map((dataList, index) => {
+        // console.log("dataList", dataList);
         return (
           <Panel
             key={index}
             header={
-              <Button width="100%" type="tertiary">
+              <Button width="100%" type={buttonType}>
                 <div className="content-button">
-                  <div className="item-img">
-                    <FontAwesomeIcon icon={requeriment.icon} />
-                  </div>
+                  {dataList.icon && (
+                    <div className="item-img">
+                      <FontAwesomeIcon icon={dataList.icon} />
+                    </div>
+                  )}
                   <div className="item-text">
                     <Text>
-                      <h3>{requeriment.title}</h3>
+                      <h3>{dataList.title}</h3>
                     </Text>
                   </div>
                 </div>
@@ -32,11 +47,41 @@ export const ShowAndHiddenButton = ({ requerimentsJace = [] }) => {
             }
             className="site-collapse-custom-panel"
           >
-            <ul className="description">
-              {requeriment.descriptions.map((description, index) => (
-                <li key={index}>{description}</li>
-              ))}
-            </ul>
+            {dataList.descriptions && (
+              <ul className="description">
+                {dataList.descriptions.map((description, index) => (
+                  <li key={index}>{description}</li>
+                ))}
+              </ul>
+            )}
+
+            {dataList.imgCarousels && (
+              <SectionCarousel>
+                <Carousel autoplay>
+                  {dataList.imgCarousels.map((imgCarousel, index) => (
+                    <ImgCarousel
+                      key={index}
+                      image={imgCarousel}
+                      alt={dataList.title}
+                    />
+                  ))}
+                </Carousel>
+              </SectionCarousel>
+            )}
+
+            {dataList.listSocialLinks && (
+              <SectionContact>
+                <h4>CONTACTOS:</h4>
+                {dataList.listSocialLinks.map((socialLink, index) => (
+                  <SocialLink
+                    key={index}
+                    icon={socialLink.icon}
+                    link={socialLink.link}
+                    text={socialLink.text}
+                  />
+                ))}
+              </SectionContact>
+            )}
           </Panel>
         );
       })}
@@ -45,34 +90,47 @@ export const ShowAndHiddenButton = ({ requerimentsJace = [] }) => {
 };
 
 const CollapseAnt = styled(Collapse)`
-  padding: 0.3em 0;
-  .content-button {
-    display: grid;
-    grid-template-columns: 10% 1fr 10%;
-    justify-items: start;
-    width: 100%;
-    grid-column-gap: 2.5em;
-    .item-img {
-      svg {
-        font-size: 2.5em;
+  ${({ contentAlign, gripTemplateColumns }) => css`
+    padding: 0.3em 0;
+    .content-button {
+      display: grid;
+      grid-template-columns: ${gripTemplateColumns};
+      justify-items: ${contentAlign};
+      width: 100%;
+      grid-column-gap: 2.5em;
+
+      .item-img {
+        svg {
+          font-size: 2.5em;
+        }
       }
-    }
-    .item-text {
-      span {
-        h3 {
-          color: #fff;
+      .item-text {
+        span {
+          h3 {
+            color: #fff;
+          }
         }
       }
     }
-  }
-  .description {
-    font-size: 1.3em;
-    line-height: 1.3em;
-    padding: 0 0.5em;
-    text-align: justify;
-    list-style: none;
-    li {
-      margin-bottom: 0.5em;
+    .description {
+      font-size: 1.3em;
+      line-height: 1.3em;
+      padding: 0 0.5em;
+      text-align: justify;
+      list-style: none;
+      li {
+        margin-bottom: 0.5em;
+      }
     }
+  `}
+`;
+
+const SectionCarousel = styled.div`
+  padding: 0.7em 0.2em;
+`;
+
+const SectionContact = styled.div`
+  h4 {
+    font-weight: 700;
   }
 `;

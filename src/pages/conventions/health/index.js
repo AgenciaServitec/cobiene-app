613d09aa78880107cdf53d340d2eb4ef309dba3a
angelgt <angel.gamonal@agenciaservitec.com>
2022-09-23 18:38:20 -0500

    add page health

---
+++ src/pages/conventions/health/index.js
@@ -0,0 +1,12 @@
+import React from "react";
+import { Button } from "../../../components";
+
+export const Health = () => {
+  return (
+    <div>
+      <h2>Salud</h2>
+      <Button width="100%">Laboratorios</Button>
+      <Button width="100%">Clinicas</Button>
+    </div>
+  );
+};

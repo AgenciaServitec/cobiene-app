3f120475473a3ee3ff5e6a45fc2d02e2834f511e
angelgt <angel.gamonal@agenciaservitec.com>
2022-09-23 18:34:24 -0500

    update data

--- src/data-list/banks.js
+++ src/data-list/banks.js
@@ -1,44 +1,77 @@
-import { Bank1, Bank2, Bank3, Bank4, Bank5 } from "../images";
+import {
+  BankComercio,
+  BankGnb,
+  BankInterbank,
+  BankPichincha,
+  BankScotiabank,
+} from "../images";
 
 export const banks = [
   {
-    image: Bank1,
+    image: BankScotiabank,
     title: "BCO. SCOTIABANK",
     benefits: [
-      "TASAS PREFERENCIALES PARA PRÉSTRAMO POR CONVENIO - COMPRA DE DEUDAS",
-      "TASAS PREFERENCIALES 12% Y 16%  PARA PRÉSTAMOS PERSONALES",
+      "ALCANCE DEL CONVENIO\n",
+      "Dirigido al personal militar en situación de actividad o retiro y al personal civil en actividad o cesante\n",
+      "TASA DE INTERES ANUAL\n",
+      "Hasta 16% desde 06 hasta 72 meses\n",
+      "Según campañas impulsadas por el banco\n",
     ],
   },
   {
-    image: Bank2,
+    image: BankComercio,
     title: "BCO. COMERCIO",
     benefits: [
-      "TASAS PREFERENCIALES PARA PRÉSTRAMO POR CONVENIO - COMPRA DE DEUDAS",
-      "TASAS PREFERENCIALES 12% Y 16%  PARA PRÉSTAMOS PERSONALES",
+      "ALCANCE DEL CONVENIO:",
+      "Dirigido al personal militar en situación de actividad o retiro y personal civil en situación de actividad o cesante.",
+      "EDAD PARA ACCEDER AL PRESTAMO:",
+      " - MINIMA 18 AÑOS",
+      " - MAXIMA 69 AÑOS 11 MESES",
+      "TASA DE INTERES EFECTIVA ANUAL: ",
+      "14.5 %.",
+      "Campañas impulsadas por el banco según el análisis del mercado",
     ],
   },
   {
-    image: Bank3,
+    image: BankGnb,
     title: "BCO. GNB ",
     benefits: [
-      "TASAS PREFERENCIALES PARA PRÉSTRAMO POR CONVENIO - COMPRA DE DEUDAS",
-      "TASAS PREFERENCIALES 12% Y 16%  PARA PRÉSTAMOS PERSONALES",
+      "ALCANCE DEL CONVENIO\n",
+      "Dirigido al personal militar en situación de actividad o retiro y personal civil en situación de actividad y cesante\n",
+      "TASA DE INTERES EFECTIVA ANUAL\n",
+      "Clasificación SBS Normal hasta 15.0% desde 06 meses hasta 72 meses\n",
+      "Clasificación SBS CPP 16.0% desde 06 meses hasta 72 meses\n",
+      "Campañas impulsadas por el banco según el análisis del mercado\n",
     ],
   },
+  // {
+  //   image: Bank4,
+  //   title: "BCO. CONTINENTAL  ",
+  //   benefits: [
+  //     "TASAS PREFERENCIALES PARA PRÉSTRAMO POR CONVENIO - COMPRA DE DEUDAS",
+  //     "TASAS PREFERENCIALES 12% Y 16%  PARA PRÉSTAMOS PERSONALES",
+  //   ],
+  // },
   {
-    image: Bank4,
-    title: "BCO. CONTINENTAL  ",
-    benefits: [
-      "TASAS PREFERENCIALES PARA PRÉSTRAMO POR CONVENIO - COMPRA DE DEUDAS",
-      "TASAS PREFERENCIALES 12% Y 16%  PARA PRÉSTAMOS PERSONALES",
-    ],
-  },
-  {
-    image: Bank5,
+    image: BankInterbank,
     title: "BCO. INTERBANK",
     benefits: [
-      "TASAS PREFERENCIALES PARA PRÉSTRAMO POR CONVENIO - COMPRA DE DEUDAS",
-      "TASAS PREFERENCIALES 12% Y 16%  PARA PRÉSTAMOS PERSONALES",
+      "ALCANCE DEL CONVENIO\n",
+      "Dirigido al personal militar en situación de actividad o retiro y al personal civil en actividad o cesante\n",
+      "TASA DE INTERES ANUAL\n",
+      "Hasta 19.5% desde 06 hasta 72 meses\n",
+      "Campañas impulsadas por el banco según análisis del mercado\n",
+    ],
+  },
+  {
+    image: BankPichincha,
+    title: "BCO. PICHINCHA",
+    benefits: [
+      "ALCANCE DEL CONVENIO\n",
+      "Dirigido al personal militar en situación de actividad o retiro y al personal civil en actividad o cesante\n",
+      "TASA DE INTERES ANUAL\n",
+      "Clasificación SBS normal Hasta 14% desde 06 hasta 72 meses\n",
+      "Campañas impulsadas por el banco de acuerdo al análisis del mercado\n",
     ],
   },
 ];

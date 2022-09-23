7500d18a2cfebc09e04ec23f6456ac40105b257f
angelgt <angel.gamonal@agenciaservitec.com>
2022-09-23 18:37:52 -0500

    add page financials

---
+++ src/pages/conventions/financials/index.js
@@ -0,0 +1,31 @@
+import React from "react";
+import styled, { css } from "styled-components";
+import { CollapseWithButtons } from "../../../components";
+import { universities } from "../../../data-list";
+
+export const Financials = () => (
+  <Container>
+    <div className="content-logo">
+      <h2>Financieras</h2>
+    </div>
+    <CollapseWithButtons dataLists={universities} />
+  </Container>
+);
+
+const Container = styled.div`
+  ${() => css`
+    width: 100%;
+    height: auto;
+    .content-logo {
+      display: flex;
+      flex-direction: column;
+      align-items: center;
+      justify-content: center;
+      margin: 1rem auto;
+
+      h2 {
+        text-align: center;
+      }
+    }
+  `}
+`;

a8c1c6fd2faeabc8dc85bdd435b68b0820430a85
angelgt <angel.gamonal@agenciaservitec.com>
2022-09-23 18:36:55 -0500

    add page education-conventions

---
+++ src/pages/conventions/education-conventions/index.js
@@ -0,0 +1,30 @@
+import React from "react";
+import { Button, Title } from "../../../components";
+
+import { useNavigate } from "react-router";
+
+export const EducationConventions = () => {
+  const navigate = useNavigate();
+  return (
+    <div>
+      <Title>Educación</Title>
+
+      <Button
+        width="100%"
+        onClick={() =>
+          navigate("/conventions/education-conventions/universities")
+        }
+      >
+        Universidades
+      </Button>
+      <Button
+        width="100%"
+        onClick={() =>
+          navigate("/conventions/education-conventions/institutes")
+        }
+      >
+        Institutos
+      </Button>
+    </div>
+  );
+};

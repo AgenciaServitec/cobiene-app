65f60da59b0526abfb57f44469183b9c1e64c867
angelgt <angel.gamonal@agenciaservitec.com>
2022-09-24 21:12:25 -0500

    config buttons

--- src/pages/conventions/health/index.js
+++ src/pages/conventions/health/index.js
@@ -1,12 +1,31 @@
 import React from "react";
 import { Button } from "../../../components";
+import { useNavigate } from "react-router";
+import styled from "styled-components";
 
 export const Health = () => {
+  const navigate = useNavigate();
   return (
-    <div>
+    <Container>
       <h2>Salud</h2>
-      <Button width="100%">Laboratorios</Button>
-      <Button width="100%">Clinicas</Button>
-    </div>
+      <Button
+        width="100%"
+        onClick={() => navigate("/conventions/health/laboratories")}
+      >
+        Laboratorios
+      </Button>
+      <Button
+        width="100%"
+        onClick={() => navigate("/conventions/health/clinics")}
+      >
+        Clinicas
+      </Button>
+    </Container>
   );
 };
+
+const Container = styled.div`
+  h2 {
+    text-align: center;
+  }
+`;

e33bcae614fb6675250db648468f37bb901892c0
angelgt <angel.gamonal@agenciaservitec.com>
2022-09-24 21:11:03 -0500

    show list "gym"

--- src/pages/conventions/companies/gym/index.js
+++ src/pages/conventions/companies/gym/index.js
@@ -1,13 +1,20 @@
 import React from "react";
 import styled, { css } from "styled-components";
+import { companies } from "../../../../data-list";
+import { CollapseWithButtons } from "../../../../components";
 
-export const Gym = () => (
-  <Container>
-    <div className="content-logo">
-      <h2>Gimnasios</h2>
-    </div>
-  </Container>
-);
+export const Gym = () => {
+  const gyms = companies.filter((com) => com.type === "gym");
+
+  return (
+    <Container>
+      <div className="content-logo">
+        <h2>Gimnasios</h2>
+      </div>
+      <CollapseWithButtons dataLists={gyms} />
+    </Container>
+  );
+};
 
 const Container = styled.div`
   ${() => css`

5ddd3ce82cefd54a8eb91a1df503bdf9d7c48964
angelgt <angel.gamonal@agenciaservitec.com>
2022-09-24 21:12:40 -0500

    show list "hotels"

--- src/pages/conventions/hotels/index.js
+++ src/pages/conventions/hotels/index.js
@@ -1,16 +1,20 @@
 import React from "react";
 import styled, { css } from "styled-components";
 import { CollapseWithButtons } from "../../../components";
-import { universities } from "../../../data-list";
+import { companies } from "../../../data-list";
 
-export const Hotels = () => (
-  <Container>
-    <div className="content-logo">
-      <h2>Hoteles</h2>
-    </div>
-    <CollapseWithButtons dataLists={universities} />
-  </Container>
-);
+export const Hotels = () => {
+  const hotel = companies.filter((com) => com.type === "hotel");
+
+  return (
+    <Container>
+      <div className="content-logo">
+        <h2>Hoteles</h2>
+      </div>
+      <CollapseWithButtons dataLists={hotel} />
+    </Container>
+  );
+};
 
 const Container = styled.div`
   ${() => css`

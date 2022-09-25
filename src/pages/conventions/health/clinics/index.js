97f85bd79efe82bde4c5a25f81300319a9bd7840
angelgt <angel.gamonal@agenciaservitec.com>
2022-09-24 21:11:38 -0500

    show list "clinics"

---
+++ src/pages/conventions/health/clinics/index.js
@@ -0,0 +1,33 @@
+import { companies } from "../../../../data-list";
+import { CollapseWithButtons } from "../../../../components";
+import styled from "styled-components";
+import React from "react";
+
+export const Clinics = () => {
+  const clinic = companies.filter((com) => com.type === "clinic");
+
+  return (
+    <Container>
+      <div className="content-logo">
+        <h2>Clínicas</h2>
+      </div>
+      <CollapseWithButtons dataLists={clinic} />
+    </Container>
+  );
+};
+
+const Container = styled.div`
+  width: 100%;
+  height: auto;
+  .content-logo {
+    display: flex;
+    flex-direction: column;
+    align-items: center;
+    justify-content: center;
+    margin: 1rem auto;
+
+    h2 {
+      text-align: center;
+    }
+  }
+`;

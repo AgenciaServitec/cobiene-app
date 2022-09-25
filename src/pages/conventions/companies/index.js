1d0233490a176ffdcd54096aa170ac1a2e8262be
angelgt <angel.gamonal@agenciaservitec.com>
2022-09-24 19:51:46 -0500

    update layout

--- src/pages/conventions/companies/index.js
+++ src/pages/conventions/companies/index.js
@@ -1,16 +1,30 @@
 import React from "react";
+import { useNavigate } from "react-router";
 import styled, { css } from "styled-components";
-import { CollapseWithButtons } from "../../../components";
-import { companies } from "../../../data-list";
+import { Button } from "../../../components";
 
-export const Companies = () => (
-  <Container>
-    <div className="content-logo">
-      <h2>Empresas</h2>
-    </div>
-    <CollapseWithButtons dataLists={companies} />
-  </Container>
-);
+export const Companies = () => {
+  const navigate = useNavigate();
+  return (
+    <Container>
+      <div className="content-logo">
+        <h2>Empresas</h2>
+      </div>
+      <Button
+        width="100%"
+        onClick={() => navigate("/conventions/companies/commercial-shop")}
+      >
+        Tiendas Comerciales
+      </Button>
+      <Button
+        width="100%"
+        onClick={() => navigate("/conventions/companies/gym")}
+      >
+        Gimnasios
+      </Button>
+    </Container>
+  );
+};
 
 const Container = styled.div`
   ${() => css`

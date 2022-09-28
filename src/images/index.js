77c322a1e8ac723b49e30952011433b0345b1bf0
angelgt <angel.gamonal@agenciaservitec.com>
2022-09-28 16:15:17 -0500

    add image

--- src/images/index.js
+++ src/images/index.js
@@ -14,6 +14,8 @@ export { default as ImgEmptyDefault } from "./webp/img-default.webp";
 export { default as EjercitoDelPeruLogo } from "./webp/ejercito-del-peru-logo.webp";
 export { default as ImgLogoSad } from "./webp/img-logo-sad.webp";
 export { default as ImgLogoJaeUpdate } from "./webp/img-jae-logo.webp";
+export { default as ImgLogoConvention } from "./webp/convention-1.webp";
+export { default as ImgLogoConvention2 } from "./webp/convention-2.webp";
 
 //--------Images Jage------
 export { default as ImgLogoJace } from "./jace/image-logo-jace.webp";

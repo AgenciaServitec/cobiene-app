13f1d21ebd2c91423444fa22345dddf34dab54b2
angelgt <angel.gamonal@agenciaservitec.com>
2022-09-29 12:18:18 -0500

    refactor code

--- src/components/ui/SortWords.js
+++ src/components/ui/SortWords.js
@@ -1,6 +1,6 @@
 import React from "react";
 
-export const SortWords = ({ words = {} }) => {
+export const SortWords = (words) => {
   const orderName = words.sort((a, b) => {
     const titleA = a.title.toLocaleLowerCase();
     const titleB = b.title.toLocaleLowerCase();

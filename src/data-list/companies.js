b9c8dd415b4ff59e6f228f9168e59cadf5bd17a4
angelgt <angel.gamonal@agenciaservitec.com>
2022-10-05 10:06:06 -0500

    correction word

--- src/data-list/companies.js
+++ src/data-list/companies.js
@@ -459,7 +459,7 @@ export const companies = [
     type: "technology",
     image: Servitec,
     title: "Servitec - Perú",
-    benefits: ["Con la presentación de su SIC 10% de descuento"],
+    benefits: ["Con la presentación de su SIP 10% de descuento"],
     links: [
       { url: "https://servitecperu.com/" },
       {

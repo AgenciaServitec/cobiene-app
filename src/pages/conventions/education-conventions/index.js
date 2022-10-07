3f201c0be62347db6cb7445f7baeca7b6536574b
angelgt <angel.gamonal@agenciaservitec.com>
2022-10-07 18:32:57 -0500

    update content

--- src/pages/conventions/education-conventions/index.js
+++ src/pages/conventions/education-conventions/index.js
@@ -1,12 +1,14 @@
 import React from "react";
 import { Button, Title } from "../../../components/public";
-
 import { useNavigate } from "react-router";
+import { FontAwesomeIcon } from "@fortawesome/react-fontawesome";
+import { faList } from "@fortawesome/free-solid-svg-icons";
+import styled from "styled-components";
 
 export const EducationConventions = () => {
   const navigate = useNavigate();
   return (
-    <div>
+    <Container>
       <Title>Educación</Title>
 
       <Button
@@ -25,6 +27,31 @@ export const EducationConventions = () => {
       >
         Institutos
       </Button>
-    </div>
+
+      <div className="list-convenios">
+        <a href="https://drive.google.com/file/d/1lKdcV-kFnP7aHESdeSZ36eJXxqgTjlwh/view?usp=sharing">
+          <FontAwesomeIcon icon={faList} size="2x" />
+          Lista de convenios de universidades e institutos:
+        </a>
+      </div>
+    </Container>
   );
 };
+
+const Container = styled.div`
+  .list-convenios {
+    color: dodgerblue;
+    text-decoration: underline;
+    text-transform: uppercase;
+    margin: 1.5rem 0;
+    font-size: 1em;
+    a {
+      margin-bottom: 1.5rem;
+      display: flex;
+      align-items: start;
+      svg {
+        margin-right: 1rem;
+      }
+    }
+  }
+`;

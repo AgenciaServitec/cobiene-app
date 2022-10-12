e831981beba789768cef7794f0496c18de1fbc53
angelgt <angel.gamonal@agenciaservitec.com>
2022-10-12 17:51:11 -0500

    refactor styles

--- src/components/public/layout/HeaderHomePage.js
+++ src/components/public/layout/HeaderHomePage.js
@@ -3,6 +3,7 @@ import { CobieneLogo, ImgCuartelCobiene } from "../../../images";
 import { FontAwesomeIcon } from "@fortawesome/react-fontawesome";
 import { faBars } from "@fortawesome/free-solid-svg-icons";
 import React from "react";
+import { mediaQuery } from "../../../styles/constants/mediaQuery";
 
 export const HeaderHomePage = ({ onSetVisibleDrawer }) => {
   return (
@@ -17,7 +18,7 @@ export const HeaderHomePage = ({ onSetVisibleDrawer }) => {
         <FontAwesomeIcon icon={faBars} size="2x" />
       </div>
       <div className="horizontal-line" />
-      <div className="wrapper-header">
+      <WrapperHeader>
         <div className="img-box">
           <img
             src={CobieneLogo}
@@ -30,7 +31,7 @@ export const HeaderHomePage = ({ onSetVisibleDrawer }) => {
           <h3>COMANDO DE BIENESTAR</h3>
           <p>Del Ejército del Perú</p>
         </div>
-      </div>
+      </WrapperHeader>
     </Container>
   );
 };
@@ -55,20 +56,53 @@ const Container = styled.header`
     height: 8px;
     background-color: #7f9e47;
   }
-  .wrapper-header {
-    width: 100%;
-    display: grid;
-    grid-gap: 0.7rem;
-    grid-template-columns: auto 1fr;
-    position: absolute;
-    top: 8.4rem;
-    padding: 0 1rem;
-    .img-box {
-      background: radial-gradient(#11581e, #041607);
-      border: 7px solid #7f9e47;
-      padding: 1rem;
-      border-radius: 50%;
+`;
 
+const WrapperHeader = styled.div`
+  width: 100%;
+  display: grid;
+  position: absolute;
+  top: 8.4rem;
+  padding: 0 1rem;
+  grid-template-columns: auto 1fr;
+  grid-gap: 0.7rem;
+  .img-box {
+    background: radial-gradient(#11581e, #041607);
+    border: 7px solid #7f9e47;
+    padding: 1rem;
+    border-radius: 50%;
+    width: 7em;
+    display: flex;
+    justify-content: center;
+    align-items: center;
+    img {
+      width: 4.3rem;
+      height: 4.3rem;
+      object-fit: contain;
+    }
+  }
+  .item-titles {
+    display: flex;
+    flex-direction: column;
+    align-items: center;
+    justify-content: end;
+    padding: 1rem 0 0 0;
+    h3,
+    p {
+      text-align: center;
+      line-height: 1rem;
+      font-size: 0.8rem;
+    }
+    h3,
+    p {
+      margin: 0;
+    }
+  }
+  ${mediaQuery.minMobileM} {
+    grid-template-columns: auto 1fr;
+    grid-gap: 0.7rem;
+    .img-box {
+      width: auto;
       img {
         width: 4.7rem;
         height: 4.7rem;
@@ -76,18 +110,11 @@ const Container = styled.header`
       }
     }
     .item-titles {
-      display: flex;
-      flex-direction: column;
-      align-items: center;
-      justify-content: end;
+      position: initial;
       padding: 1rem 0 0 0;
       h3 {
         line-height: 1.7rem;
         font-size: 1rem;
-      }
-      h3,
-      p {
-        margin: 0;
       }
     }
   }

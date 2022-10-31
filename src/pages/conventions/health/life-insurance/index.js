c1102107ee30e01cf8c729385c4f26e6b95e5327
angelgt <angel.gamonal@agenciaservitec.com>
2022-10-31 15:58:32 -0500

    update content

--- src/pages/conventions/health/life-insurance/index.js
+++ src/pages/conventions/health/life-insurance/index.js
@@ -1,40 +1,19 @@
 import React from "react";
 import { companies } from "../../../../data-list";
-import { InitialContent, SortWords } from "../../../../components/public";
-import styled from "styled-components";
-import { CobieneLogo } from "../../../../images";
+import {
+  CollapseWithButtons,
+  InitialContent,
+  SortWords,
+} from "../../../../components/public";
 
 export const LifeInsurance = () => {
   const lives = companies.filter((com) => com.type === "life-insurance");
 
   SortWords(lives);
   return (
-    <Container>
-      <InitialContent
-        img={CobieneLogo}
-        title="Estamos trabajando por el bienestar"
-      />
-      <InitialContent title="Proximamente..." />
-      {/*<div className="content-logo">
-        <h2>Seguros</h2>
-      </div>
-      <CollapseWithButtons dataLists={lives} />*/}
-    </Container>
+    <>
+      <InitialContent title="Seguros" />
+      <CollapseWithButtons dataLists={lives} />
+    </>
   );
 };
-
-const Container = styled.div`
-  width: 100%;
-  height: auto;
-  .content-logo {
-    display: flex;
-    flex-direction: column;
-    align-items: center;
-    justify-content: center;
-    margin: 1rem auto;
-
-    h2 {
-      text-align: center;
-    }
-  }
-`;

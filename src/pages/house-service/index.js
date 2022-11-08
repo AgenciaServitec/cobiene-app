32baf80e32d133d5da7ed5e035f051b58b1736c2
angelgt <angel.gamonal@agenciaservitec.com>
2022-11-08 12:08:58 -0500

    refactor page

--- src/pages/house-service/index.js
+++ src/pages/house-service/index.js
@@ -4,7 +4,6 @@ import {
   InitialContent,
   NavigateGoToLink,
 } from "../../components/public";
-import styled from "styled-components";
 import { CobieneLogo } from "../../images";
 import { useNavigate } from "react-router";
 
@@ -12,26 +11,21 @@ export const HouseService = () => {
   const navigate = useNavigate();
 
   return (
-    <Container>
+    <>
       <InitialContent title="Vivienda" img={CobieneLogo} />
 
       <Button
         width="100%"
         onClick={() => navigate("/house-service/house-services")}
       >
-        Casa de Servicio
+        Casa de servicio
       </Button>
       <Button
         width="100%"
         onClick={() => NavigateGoToLink("https://fovime.com/")}
       >
-        Fovime
+        FOVIME
       </Button>
-    </Container>
+    </>
   );
 };
-
-const Container = styled.div`
-  width: 100%;
-  height: auto;
-`;

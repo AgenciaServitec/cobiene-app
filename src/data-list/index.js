d5449f1f6ebc53203d5b8fe14f8124337ae31728
angelgt <angel.gamonal@agenciaservitec.com>
2022-11-10 14:44:27 -0500

    remove banks

--- src/data-list/index.js
+++ src/data-list/index.js
@@ -5,7 +5,6 @@ export * from "./universities";
 export * from "./institutes";
 export * from "./pre-academies";
 export * from "./companies";
-export * from "./banks";
 export * from "./requerimentsJace";
 export * from "./campusCircleMilitary";
 export * from "./campusTechniciansMilitary";

3e4614a108b90caa9b45abb4d947675abe8bc36b
angelgamonal <angelgamonal.t@gmail.com>
2024-02-05 14:31:09 -0500

    remove iframe

--- src/components/public/home/IframeFb.js
+++ src/components/public/home/IframeFb.js
@@ -1,18 +1,7 @@
 import styled from "styled-components";
 
 export const IframeFb = () => {
-  return (
-    <Container>
-      <iframe
-        src="https://www.facebook.com/plugins/page.php?href=https%3A%2F%2Fwww.facebook.com%2FBienestarEP&tabs=timeline&width=340&height=500&small_header=false&adapt_container_width=true&hide_cover=false&show_facepile=true&appId"
-        height="500"
-        scrolling="no"
-        frameBorder="0"
-        allowFullScreen="true"
-        allow="autoplay; clipboard-write; encrypted-media; picture-in-picture; web-share"
-      />
-    </Container>
-  );
+  return <Container></Container>;
 };
 const Container = styled.div`
   width: 100%;
